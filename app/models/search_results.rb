2a644c81f5cbf9ef434f29e6c206cb2d380288f9
jason mayer + daniel errante <mayerja@wwt.com>
2015-06-23 16:13:49 -0500

    wrapping search results in a class

--- app/models/search_results.rb
+++ app/models/search_results.rb
@@ -1,8 +1,25 @@
 class SearchResults
   attr_reader :query, :results
 
-  def initialize(query, results)
+  def initialize(query, raw_response)
     @query = query
-    @results = results
+    @raw_response = raw_response
+    @results = raw_response.fetch(:results, [])
+  end
+
+  def error?
+    @raw_response.has_key?(:error)
+  end
+
+  def meta
+    @raw_response.fetch(:meta, {})
+  end
+
+  def meta_results
+    meta.fetch(:results, {})
+  end
+
+  def total
+    meta_results.fetch(:total, 0)
   end
 end
--- spec/models/search_results_spec.rb
+++ spec/models/search_results_spec.rb
@@ -2,9 +2,12 @@ require 'rails_helper'
 
 describe SearchResults do
   let(:query) { double 'query' }
+  let(:raw_response) { { results: results, meta: meta } }
+  let(:meta) { { results: { skip: 0, limit: 1, total: total } } }
+  let(:total) { double 'total' }
   let(:results) { double 'search results from api' }
 
-  subject { described_class.new(query, results) }
+  subject { described_class.new(query, raw_response) }
 
   describe '#query' do
     it 'returns original query' do
@@ -15,6 +18,24 @@ describe SearchResults do
   describe '#results' do
     it 'returns results' do
       expect(subject.results).to eq(results)
+    end
+  end
+
+  describe '#total' do
+    it 'gets total results' do
+      expect(subject.total).to eq(total)
+    end
+  end
+
+  describe '#error' do
+    let(:raw_response) { { error: { code: "NOT_FOUND", message: "No matches found!" } } }
+
+    it 'sets total to 0' do
+      expect(subject.total).to eq(0)
+    end
+
+    it 'has an error' do
+      expect(subject.error?).to eq(true)
     end
   end
 end
