b123bdfa1990c9c350afd998a76adff08cdcb3ed
jason mayer & kenny ortmann <mayerja@wwt.com>
2015-06-29 12:55:49 -0500

    Changing warnings to use the panel partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,9 +3,9 @@ module ApplicationHelper
     field.join(', ') if field
   end
 
-  def render_panel(field_name: '', field_data: '')
+  def render_panel(field_name: '', field_data: '', extra_class: '')
     @panel_number = (@panel_number || 0) + 1
-    render(partial: 'shared/panel', locals: { field_name: field_name, field_data: field_data })
+    render(partial: 'shared/panel', locals: { field_name: field_name, field_data: field_data, extra_class: extra_class })
   end
 
   def panel_id
